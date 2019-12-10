88cc0999f32c98347e3d889a628860353e22058c
alexthefatcat <46425807+alexthefatcat@users.noreply.github.com>
2019-12-10 09:58:43 +0000

    Update async_await_tutorial.py

--- Advanced_Python/async_await_tutorial.py
+++ Advanced_Python/async_await_tutorial.py
@@ -37,45 +37,58 @@
 #
 ############################################################################
 
-import asyncio
+mode = ("sync","async")[1]
+print(mode)
 
-## Function to Workon
-async def find_divisibilites(inrange,div_by,name=""):
-    print(f"Finding nums in range {inrange}, divisible by {div_by}")
+if mode =="sync":
+    def find_divisibles(inrange, div_by):
+        print("finding nums in range {} divisible by {}".format(inrange, div_by))
+        located = []
+        for i in range(inrange):
+            if i % div_by == 0:
+                located.append(i)
+        print("Done w/ nums in range {} divisible by {}".format(inrange, div_by))
+        return located
+    def main():
+        divs1 = find_divisibles(508000, 34113)
+        divs2 = find_divisibles(100052, 3210)
+        divs3 = find_divisibles(500, 3)
+    if __name__ == '__main__':
+        main()
+
+########################################################################################
+
+if mode =="async":
+
+    import asyncio
+    ## Function to Workon
+    async def find_divisibilites(inrange,div_by,name=""):
+        print(f"Finding nums in range {inrange}, divisible by {div_by}")
+        
+        located =[]
+        for i in range(inrange):
+            if i % div_by ==0:
+                located.append(i)
+            if i % 50_000 ==0 :
+                print(f"      >>Sleep  {name}")
+                await asyncio.sleep(0.001)
+        print("  >> Finished")
+        return  located     
+         
+    async def main():
+        divs1 = loop.create_task( find_divisibilites(500_000,34,"big"   ))
+        divs2 = loop.create_task( find_divisibilites(5000   ,33,"medium"))
+        divs3 = loop.create_task( find_divisibilites(500    ,31,"small" ))
+        await asyncio.wait([divs1,divs2,divs3])
+        return divs1, divs2, divs3 
     
-    located =[]
-    for i in range(inrange):
-        if i % div_by ==0:
-            located.append(i)
-        if i % 50_000 ==0 :
-            print(f"      >>Sleep  {name}")
-            await asyncio.sleep(0.0001)
-    print("  >> Finished")
-    return  located     
-       
-
-     
-async def main():
-    divs1 = loop.create_task( find_divisibilites(500000,34,"big"))
-    divs2 = loop.create_task( find_divisibilites(5000  ,33,"medium"))
-    divs3 = loop.create_task( find_divisibilites(500   ,31,"small"))
-    await asyncio.wait([divs1,divs2,divs3])
-    return divs1, divs2, divs3 
-
-if __name__=="__main__":
-    try:
+    if __name__=="__main__":
         loop = asyncio.get_event_loop()
-        d1,d2,d3 = loop.run_until_complete(main)
+        loop.set_debug(1)   # useful for debuggin
+        d1,d2,d3 = loop.run_until_complete(main())
+        print(d1.result())
+        loop.close()    
         print("Finished**")
-    except :
-       pass
-    finally:
-        loop.close()   
-        
-        
-   # main()
-   
-   
-   
+
    
    
